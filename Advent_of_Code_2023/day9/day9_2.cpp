4f8712d18813bf4b95eba2d5db6b925178acba73
Tanasescu Andrei <artanfast@gmail.com>
2023-12-09 20:23:48 +0200

    Create day9_2.cpp

---
+++ Advent_of_Code_2023/day9/day9_2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#define pb push_back
+using namespace std;
+ifstream fin ("day9.in");
+ofstream fout ("day9.out");
+typedef long long ll;
+
+string s;
+ll sol;
+vector<ll> v;
+
+int main(){
+    while (getline(fin, s)){
+        ll i=0, sum=0;
+        while (i<s.size()){
+            ll nr=0, sgn=1;
+            if (s[i]=='-'){
+                sgn=-1;
+                i++;
+            }
+            while (i<s.size() && s[i]!=' '){
+                nr=nr*10+sgn*(s[i]-'0');
+                i++;
+            }
+            v.push_back(nr);
+            nr=0;
+            while (i<s.size() && s[i]==' ')
+                i++;
+        }
+        bool ok=1;
+        int step=0;
+        while (ok){
+            if (step%2==0)
+                sum+=v[0];
+            else
+                sum-=v[0];
+            step++;
+            ok=0;
+            for (ll i=0; i<v.size()-1; i++){
+                v[i]=v[i+1]-v[i];
+                if (v[i]!=0)
+                    ok=1;
+            }
+            v.pop_back();
+        }
+        sol+=sum;
+        v.clear();
+    }
+    fout<<sol;
+
+    return 0;
+}
